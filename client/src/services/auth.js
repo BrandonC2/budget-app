a4de733e5c43fc58fa16aa6b6b9c50417cbd2be5
BrandonC2 <98776493+BrandonC2@users.noreply.github.com>
2025-05-20 19:38:48 -0700

    found issue to frontend and backend connectivity

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://budget-app-56om.onrender.com/api';
 
 export const login = async (email, password) => {
   try {
